1a192f30ff11fcbfb09f37bb190f440c1efdc646
jujulilia <jujuscosta07@gmail.com>
2024-02-22 15:56:41 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Text, View } from "react-native";
+import LoginCliente from "./src/login/LoginCliente";
+import LoginAdm from "./src/login/LoginAdm";
+import LoginProfissional from "./src/login/LoginProfissional";
+function App(): JSX.Element {
+
+  return(
+   //<LoginCliente/>
+
+   //<LoginProfissional />
+
+   <LoginAdm/>
+  );
+
+}
+export default App;
